f58fab2f59915e1b27d4cf33acc2debdd115e334
Ricky Kawagishi <rintaro.kawagishi@gmail.com>
2021-10-15 16:23:14 +0100

    Run program. Fix any errors.

--- convolution.py
+++ convolution.py
@@ -3,7 +3,7 @@ import numpy as np
 def conv_1D_equal_length(a, b):
 	if (len(a) != len(b)):
 		raise Exception('The lists in 1D convolution must have equal lengths')
-		
+
 	result = 0
 	for a_index, a_each in enumerate(a):
 		result += a_each * b[- a_index - 1]
@@ -13,7 +13,7 @@ def conv_1D(a, b):
 	if (len(a) == len(b)):
 		return conv_1D_equal_length(a, b)
 	
-	length_a = len(a), length_b = len(b)
+	length_a, length_b = len(a), len(b)
 	if length_a < length_b:
 		list_short = a
 		list_long = b
@@ -27,7 +27,7 @@ def conv_1D(a, b):
 
 	result = []
 	for start_index_long in range(0, length_long - length_short + 1):
-		result.append(conv_1D_equal_length(list_short, list_long[start_index_long, start_index_long + length_short]))
+		result.append(conv_1D_equal_length(list_short, list_long[start_index_long:start_index_long + length_short]))
 
 	return result
 
@@ -38,15 +38,16 @@ def conv_1D2D(list_1d, list_2d, direction=0):
 			list_2d.shape[1]-len(list_1d)+1
 		)
 		result = np.empty(result_shape)
-		for index, row in enumerate(list_2d):
-			result[index] = conv_1D(list_1d, row)
+		for index in range(result_shape[0]):
+			result[index] = conv_1D(list_1d, list_2d[index])
 		return result
 	else: # y direction
 		return conv_1D2D(list_1d, list_2d.T, 0).T
 
 def conv_2D_gaussian(list_2d, sigma, kernel_size):
 	kernel_1d = gaussian_kernel_1d(sigma, kernel_size)
-	rows = conv_1D2D(kernel_1d, conv_1D2D(kernel_1d, list_2d, 0), 1)
+	result1 = conv_1D2D(kernel_1d, list_2d, 0)
+	rows = conv_1D2D(kernel_1d, result1, 1)
 	return rows
 
 
@@ -61,7 +62,7 @@ def gaussian_kernel_1d(sigma, size):
 	return kernel
 
 def gaussian_1d(x, sigma):
-	return 1 / (np.sqrt(2 * np.pi) * sigma) * np.exp(- x^2 / 2 * sigma^2)
+	return 1 / (np.sqrt(2 * np.pi) * sigma) * np.exp(- x**2 / 2 * sigma**2)
 
 # def generate_gaussian_kernel(size, sigma):
 # 	if size % 2 == 0:
@@ -78,7 +79,7 @@ def gaussian_1d(x, sigma):
 # 	return rows
 
 # def gaussian_2d(x, y, sigma):
-# 	return (1 / (2 * np.pi * sigma^2)) * np.exp( - (x^2 + y^2) / (2 * sigma^2))
+# 	return (1 / (2 * np.pi * sigma**2)) * np.exp( - (x**2 + y**2) / (2 * sigma**2))
 
 
 
--- main.py
+++ main.py
@@ -1,7 +1,28 @@
+from PIL import Image
+import numpy as np
 import convolution as conv
 
 def main():
-	print('Hello World')
+	image = Image.open('hiro.png')
+	pixels = image.load()
+	print('image loaded')
+	size = image.size
+	r, g, b, a = np.empty(size), np.empty(size), np.empty(size), np.empty(size)
+	for i in range(size[0]):
+		for j in range(size[1]):
+			r[i, j] = pixels[i, j][0]
+			g[i, j] = pixels[i, j][1]
+			b[i, j] = pixels[i, j][2]
+			a[i, j] = pixels[i, j][3]
+	print('image data loaded to arrays')
+	r_blur = conv.conv_2D_gaussian(r, 20, 51)
+	print('Blurred R')
+	g_blur = conv.conv_2D_gaussian(g, 20, 51)
+	print('Blurred G')
+	b_blur = conv.conv_2D_gaussian(b, 20, 51)
+	print('Blurred B')
+	a_blur = conv.conv_2D_gaussian(a, 20, 51)
+	print('Blurred A')
 	pass
 
 if __name__ == '__main__':
