9a7247869459d3265ebc171f7a76f3797ff4e76b
Ricky Kawagishi <rintaro.kawagishi@gmail.com>
2021-10-17 12:22:53 +0100

    minor change

--- main.py
+++ main.py
@@ -7,16 +7,12 @@ def getImageFromPixels(r, g, b, a):
 	pixels = np.empty(shape, dtype=(np.uint8, 4))
 	for j in range(shape[0]):
 		for i in range(shape[1]):
-			if a is None:
-				pixels[j, i] = (r[j, i], g[j, i], b[j, i], 255)
-			else:
-				pixels[j, i] = (r[j, i], g[j, i], b[j, i], a[j, i])
-
+			pixels[j, i] = (r[j, i], g[j, i], b[j, i], 255 if a is None else a[j, i])
 	image = Image.fromarray(pixels, 'RGBA')
 	return image
 
 def main():
-	image = Image.open('beach_200.jpg')
+	image = Image.open('data/beach_200.jpg')
 	pixels = image.load()
 	print('image loaded')
 	size = (image.size[1], image.size[0]) # (height, width)
@@ -33,7 +29,7 @@ def main():
 			if pixel_data_length == 4:
 				a[j, i] = pixels[i, j][3]
 
-	sigma, kernel_size = 3, 23
+	sigma, kernel_size = 5, 33
 	print('image data loaded to arrays')
 	r_blur = conv.conv_2D_gaussian(r, sigma, kernel_size)
 	print('Blurred R')
